69aeeb34370882bf76a1b27c14832a190965feba
liyao <929306985@qq.com>
2019-04-21 22:01:11 +0800

    代码修改

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -29,7 +29,7 @@
                        aria-haspopup="true" aria-expanded="false">{{Auth::user()->name}} <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <li><a href="/user/{{Auth::user()->id}}">我的主页</a></li>
-                        <li><a href="/user/5/setting">个人设置</a></li>
+                        <li><a href="/user/{{Auth::user()->id}}/setting">个人设置</a></li>
                         <li><a href="/logout">登出</a></li>
                     </ul>
                 </div>
