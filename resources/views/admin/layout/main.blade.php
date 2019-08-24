13d642a4239c4815f4896aa64e3129a176df952e
liyao <liyao55201@gmail.com>
2019-08-24 11:15:30 +0800

    引用js

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -9,28 +9,28 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
-    <link rel="stylesheet" href="/adminlte/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/bootstrap/css/bootstrap.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="/adminlte/dist/css/AdminLTE.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/dist/css/AdminLTE.min.css">
     <!-- AdminLTE Skins. Choose a skin from the css/skins
          folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="/adminlte/dist/css/skins/_all-skins.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/dist/css/skins/_all-skins.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="/adminlte/plugins/iCheck/flat/blue.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/plugins/iCheck/flat/blue.css">
     <!-- Morris chart -->
-    <link rel="stylesheet" href="/adminlte/plugins/morris/morris.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/plugins/morris/morris.css">
     <!-- jvectormap -->
-    <link rel="stylesheet" href="/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
     <!-- Date Picker -->
-    <link rel="stylesheet" href="/adminlte/plugins/datepicker/datepicker3.css">
+    <link rel="stylesheet" href=`adminlte/plugins/datepicker/datepicker3.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="/adminlte/plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/plugins/daterangepicker/daterangepicker.css">
     <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('/') }}adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -61,7 +61,7 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 2.2.3 -->
-<script src="/adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/jQuery/jquery-3.4.1.js"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
@@ -69,34 +69,34 @@
     $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.6 -->
-<script src="/adminlte/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/bootstrap/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-<script src="/adminlte/plugins/morris/morris.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/morris/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="/adminlte/plugins/sparkline/jquery.sparkline.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/sparkline/jquery.sparkline.min.js"></script>
 <!-- jvectormap -->
-<script src="/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="/adminlte/plugins/knob/jquery.knob.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/knob/jquery.knob.js"></script>
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-<script src="/adminlte/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="/adminlte/plugins/datepicker/bootstrap-datepicker.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/datepicker/bootstrap-datepicker.js"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
 <!-- Slimscroll -->
-<script src="/adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="/adminlte/plugins/fastclick/fastclick.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/plugins/fastclick/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="/adminlte/dist/js/app.min.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/dist/js/app.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/adminlte/dist/js/pages/dashboard.js"></script>
+<script src="{{ URL::asset('/') }}adminlte/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="/adminlte/dist/js/demo.js"></script>
-<script src="{{ mix('/js/admin.js') }}"></script>
+<script src="{{ URL::asset('/') }}adminlte/dist/js/demo.js"></script>
+<script src="{{ URL::asset('/') }}js/admin.js "></script>
 </body>
 </html>
